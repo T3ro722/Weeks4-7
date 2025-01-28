da27623f1007f1b8e7c0e135479deb3901ddf114
Qiaoya Fang <fangqi@shernet.sheridancollege.ca>
2025-01-28 17:11:26 -0500

    Added sound

--- Assets/Scripts/EnableDisable.cs
+++ Assets/Scripts/EnableDisable.cs
@@ -7,6 +7,8 @@ public class EnableDisable : MonoBehaviour
     public GameObject go;
     public SpriteRenderer sr;
     public EnableDisable script;
+    public AudioSource audioSource;
+    public AudioClip clip;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +27,13 @@ public class EnableDisable : MonoBehaviour
         {
             sr.enabled = true;
             go.SetActive(false);
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if (audioSource.isPlaying == false)
+            {
+                audioSource.PlayOneShot(clip);
+            }
         }
     }
 
