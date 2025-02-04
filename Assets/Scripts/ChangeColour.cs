eaabe6a597b36c66d39806f4c9fd7ee034a23524
RIKKIKIISU\tnggr <helenafang00@gmail.com>
2025-02-04 17:34:07 -0500

    added button, click to change sprite to random color

---
+++ Assets/Scripts/ChangeColour.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColour : MonoBehaviour
+{
+    public SpriteRenderer targetSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Changecolor();
+        }
+        void Changecolor()
+        {
+            Color randomcolour = new Color(Random.value, Random.value, Random.value);
+            targetSprite.color = randomcolour;
+        }
+    }
+}
+
