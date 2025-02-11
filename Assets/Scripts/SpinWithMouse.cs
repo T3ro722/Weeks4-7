9a99fc55b641e77002d6b7e866d8fc5c2a47a5c7
Qiaoya Fang <fangqi@shernet.sheridancollege.ca>
2025-02-11 17:15:50 -0500

    Makes the first coding gym challenge, make tank move wth WASD, bullet shoots out with mouse, barrel move with mouse

---
+++ Assets/Scripts/SpinWithMouse.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinWithMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - (Vector2)transform.position;
+        transform.right = direction;
+    }
+}
